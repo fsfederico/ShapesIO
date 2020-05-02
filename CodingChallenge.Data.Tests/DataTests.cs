84ee9efe7d0266401328ce6720f01e3fa01d0f9d
Federico Gabriel Fia Sare <fedefiasare@gmail.com>
2020-05-02 17:07:47 -0300

    Abstracción de la presentacion de los calculos

--- CodingChallenge.Data.Tests/DataTests.cs
+++ CodingChallenge.Data.Tests/DataTests.cs
@@ -13,14 +13,14 @@ namespace CodingChallenge.Data.Tests
         public void TestResumenListaVacia()
         {
             Assert.AreEqual("<h1>Lista vacía de formas!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>(), Languages.Castellano.GetCode()));
+                CalculadoraGeometrica.Imprimir(new List<FormaGeometrica>(), Languages.Castellano.GetCode()));
         }
 
         [TestCase]
         public void TestResumenListaVaciaFormasEnIngles()
         {
             Assert.AreEqual("<h1>Empty list of shapes!</h1>",
-                FormaGeometrica.Imprimir(new List<FormaGeometrica>()));
+                CalculadoraGeometrica.Imprimir(new List<FormaGeometrica>()));
         }
 
         [TestCase]
@@ -28,7 +28,7 @@ namespace CodingChallenge.Data.Tests
         {
             var cuadrados = new List<FormaGeometrica> { new Cuadrado(5) };
 
-            var resumen = FormaGeometrica.Imprimir(cuadrados, Languages.Castellano.GetCode());
+            var resumen = CalculadoraGeometrica.Imprimir(cuadrados, Languages.Castellano.GetCode());
 
             Assert.AreEqual("<h1>Reporte de Formas</h1>1 Cuadrado | Area 25 | Perimetro 20 <br/>TOTAL:<br/>1 formas Perimetro 20 Area 25", resumen);
         }
@@ -43,7 +43,7 @@ namespace CodingChallenge.Data.Tests
                 new Cuadrado(3)
             };
 
-            var resumen = FormaGeometrica.Imprimir(cuadrados, Languages.Ingles.GetCode());
+            var resumen = CalculadoraGeometrica.Imprimir(cuadrados, Languages.Ingles.GetCode());
 
             Assert.AreEqual("<h1>Shapes report</h1>3 Squares | Area 35 | Perimeter 36 <br/>TOTAL:<br/>3 shapes Perimeter 36 Area 35", resumen);
         }
@@ -62,7 +62,7 @@ namespace CodingChallenge.Data.Tests
                 new TrianguloEquilatero(4.2m)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, Languages.Ingles.GetCode());
+            var resumen = CalculadoraGeometrica.Imprimir(formas, Languages.Ingles.GetCode());
 
             Assert.AreEqual(
                 "<h1>Shapes report</h1>2 Squares | Area 29 | Perimeter 28 <br/>2 Circles | Area 13,01 | Perimeter 18,06 <br/>3 Triangles | Area 49,64 | Perimeter 51,6 <br/>TOTAL:<br/>7 shapes Perimeter 97,66 Area 91,65",
@@ -83,7 +83,7 @@ namespace CodingChallenge.Data.Tests
                 new TrianguloEquilatero(4.2m)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, Languages.Castellano.GetCode());
+            var resumen = CalculadoraGeometrica.Imprimir(formas, Languages.Castellano.GetCode());
 
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>2 Cuadrados | Area 29 | Perimetro 28 <br/>2 Círculos | Area 13,01 | Perimetro 18,06 <br/>3 Triángulos | Area 49,64 | Perimetro 51,6 <br/>TOTAL:<br/>7 formas Perimetro 97,66 Area 91,65",
@@ -98,7 +98,7 @@ namespace CodingChallenge.Data.Tests
                 new Rectangulo(2,4)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, Languages.Castellano.GetCode());
+            var resumen = CalculadoraGeometrica.Imprimir(formas, Languages.Castellano.GetCode());
 
             Assert.AreEqual(
                 "<h1>Reporte de Formas</h1>1 Rectángulo | Area 8 | Perimetro 12 <br/>TOTAL:<br/>1 formas Perimetro 12 Area 8",
@@ -115,7 +115,7 @@ namespace CodingChallenge.Data.Tests
                 new Trapecio(3,3,6)
             };
 
-            var resumen = FormaGeometrica.Imprimir(formas, Languages.Portugues.GetCode());
+            var resumen = CalculadoraGeometrica.Imprimir(formas, Languages.Portugues.GetCode());
 
             Assert.AreEqual(
                 "<h1>Relatório de formas</h1>3 Trapézios | Área 21 | Perímetro 31,42 <br/>TOTAL:<br/>3 formas Perímetro 31,42 Área 21",
--- CodingChallenge.Data/Classes/FormaGeometrica.cs
+++ CodingChallenge.Data/Classes/FormaGeometrica.cs
@@ -7,69 +7,12 @@
  * TODO: Implementar Trapecio/Rectangulo, agregar otro idioma a reporting.
  * */
 
-using CodingChallenge.Data.Extensions;
-using CodingChallenge.Data.Resources;
-using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading;
 
 namespace CodingChallenge.Data.Classes
 {
     public abstract class FormaGeometrica
     {
-        public static string Imprimir(List<FormaGeometrica> formas, string idioma = "")
-        {
-            if (string.IsNullOrEmpty(idioma))
-            {
-                idioma = Languages.Ingles.GetCode();
-            }
-
-            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture(idioma);
-
-            var sb = new StringBuilder();
-
-            if (!formas.Any())
-            {
-                sb.Append($"<h1>{Messages.EmptyListOfShapes}</h1>");
-
-            }
-            else
-            {
-                // Hay por lo menos una forma
-                // HEADER
-                sb.Append($"<h1>{Messages.ShapesReport}</h1>");
-
-                foreach (var t in Type.GetTypeArray(formas.ToArray()).Distinct())
-                {
-                    var formasPorTipo = formas.Where(f => f.GetType() == t);
-                    var area = formasPorTipo.Sum(fpt => fpt.CalculateArea());
-                    var perimetro = formasPorTipo.Sum(fpt => fpt.CalculatePerimeter());
-                    sb.Append(ObtenerLinea(formasPorTipo.FirstOrDefault().GetQuantityDescription(formasPorTipo.ToList()), area, perimetro));
-                }
-
-                // FOOTER
-                sb.Append("TOTAL:<br/>");
-                sb.Append($"{formas.Count} { Messages.Shapes.ToLower()} ");
-                sb.Append($"{Messages.Perimeter} {formas.Sum(f => f.CalculatePerimeter()):#.##} ");
-                sb.Append($"{Messages.Area} {formas.Sum(f => f.CalculateArea()):#.##}");
-            }
-
-            return sb.ToString();
-        }
-
-        private static string ObtenerLinea(string cantidad, decimal area, decimal perimetro)
-        {
-            if (!string.IsNullOrEmpty(cantidad))
-            {
-                return $"{cantidad} | {Messages.Area} {area:#.##} | {Messages.Perimeter} {perimetro:#.##} <br/>";
-            }
-
-            return string.Empty;
-        }
-
         public abstract decimal CalculateArea();
 
         public abstract decimal CalculatePerimeter();
